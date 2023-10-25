c9f0578294872b0e1695e940a0f2f80a79db6a82
Saba  Bibi <113670621+SabaBibi62@users.noreply.github.com>
2023-10-25 16:33:08 +0500

    Add files via upload

---
+++ Saba_Question2/CC_Mid_Question2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CC_Mid_Question2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ParseButton_Click(object sender, EventArgs e)
+        {
+            string input = InputTextBox.Text;
+            LL1Parser parser = new LL1Parser(input);
+            parser.Parse();
+            if (parser.currentPosition == input.Length)
+            {
+                ResultLabel.Text = "The input is valid.";
+            }
+            else
+            {
+                ResultLabel.Text = "The input is invalid.";
+            }
+        }
+    }
+}
---
+++ Saba_Question2/CC_Mid_Question2/LL1Parser.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC_Mid_Question2
+{
+    public class LL1Parser
+    {
+        private readonly string input;
+        public int currentPosition;
+
+        public LL1Parser(string input)
+        {
+            this.input = input;
+            currentPosition = 0;
+        }
+
+        public void Parse()
+        {
+            S();
+        }
+
+        private void S()
+        {
+            E();
+            Match('$');
+        }
+
+        private void E()
+        {
+            T();
+            EPrime();
+        }
+
+        private void EPrime()
+        {
+            if (Match('+'))
+            {
+                T();
+                EPrime();
+            }
+        }
+
+        private void T()
+        {
+            F();
+            TPrime();
+        }
+
+        private void TPrime()
+        {
+            if (Match('*'))
+            {
+                F();
+                TPrime();
+            }
+        }
+
+        private void F()
+        {
+            if (Match('('))
+            {
+                E();
+                Match(')');
+            }
+            else
+            {
+                Match('i');
+                Match('d');
+            }
+        }
+
+        private bool Match(char expected)
+        {
+            if (currentPosition >= input.Length)
+            {
+                return false;
+            }
+
+            if (input[currentPosition] == expected)
+            {
+                currentPosition++;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
